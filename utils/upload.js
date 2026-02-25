f634ac6654830e0f8bacd4c94240c9d1e1460ef2
Abhishek Mishra <abhi@Abhisheks-MacBook-Air.local>
2026-02-25 23:30:37 +0530

    first commit

---
+++ utils/upload.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const multer = require("multer");
+const CloudinaryStorage = require("multer-storage-cloudinary").CloudinaryStorage;
+const cloudinary = require("../config/cloudinary.js");
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "disaster-relief-app",
+    allowed_formats: ["jpg", "png", "jpeg", "webp"],
+  },
+});
+
+const upload = multer({ storage });
+
+module.exports = upload;
